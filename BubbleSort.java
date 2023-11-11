220323fafddb4d5c239b81b46cc57906fca6000a
shahvezjumani <sha685789@gmail.com>
2023-11-11 18:17:21 +0500

    code of bubble sort method

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,5 +1,19 @@
 package SortingAlgorithms;
-
 public class BubbleSort {
+    public static void main(String[] args) {
+        int [] arr = {3,1,2,-3,43,-100};
+        sort(arr);
+    }
+    public static void sort(int [] arr){
+        for(int i=0;i< arr.length - 1;i++){
+            for(int j=1;j< arr.length - i;j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
 }
 

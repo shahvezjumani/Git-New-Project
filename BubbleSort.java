efc82e577e3a8cc7594260620735c6b43b3beb41
shahvezjumani <sha685789@gmail.com>
2023-11-11 18:37:28 +0500

    one line is deleted

--- BubbleSort.java
+++ BubbleSort.java
@@ -9,7 +9,6 @@ public class BubbleSort {
             for(int j=1;j< arr.length - i;j++){
                 if(arr[j] < arr[j-1]){
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
                     arr[j-1] = temp;
                 }
             }
